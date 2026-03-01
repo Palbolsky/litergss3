ec046473023bb9cba87662da52406f4e80ad32d6
Ota <stevenotaku17@gmail.com>
2026-03-01 10:43:46 +0100

    Add support for subdirectories in the extension build system.
    - extconf.rb now recursively discovers all .cpp source files using Dir.glob
    - Subdirectories are automatically added to $VPATH and $INCFLAGS
    - ext/ parent directory added to include paths to support LiteRGSS/ prefixed includes
    - Moved Window.cpp and Window.h into window/ subdirectory

--- ext/LiteRGSS/extconf.rb
+++ ext/LiteRGSS/extconf.rb
@@ -3,26 +3,29 @@ require 'mkmf'
 
 ext_name = 'LiteRGSS'
 
-# Raylib version to download: raylib-5.5_win32_mingw-w64
-
-# Relative paths to raylib
 raylib_dir = File.expand_path('../../vendor/raylib', __dir__)
 raylib_include = File.join(raylib_dir, 'include')
 raylib_lib = File.join(raylib_dir, 'lib')
 
-# Add the paths
 $INCFLAGS << " -I#{raylib_include}"
 $LIBPATH << raylib_lib
 
-# Check if raylib exists
 abort "raylib.h not found" unless File.exist?(File.join(raylib_include, 'raylib.h'))
 abort "libraylib.a not found" unless File.exist?(File.join(raylib_lib, 'libraylib.a'))
 
 have_library('raylib')
-
-# Required Windows system libraries
 $LDFLAGS << ' -lraylib -lopengl32 -lgdi32 -lwinmm'
-
 $CXXFLAGS << ' -std=c++17'
+
+src_dir = __dir__
+
+Dir.glob("#{src_dir}/**/").each do |subdir|
+  next if subdir == src_dir + '/'
+  rel = subdir.sub(src_dir + '/', '')
+  $VPATH << "$(srcdir)/#{rel}"
+  $INCFLAGS << " -I#{subdir}"
+end
+
+$srcs = Dir.glob("#{src_dir}/**/*.cpp").map { |f| f.sub(src_dir + '/', '') }.sort
 
 create_makefile(ext_name)
