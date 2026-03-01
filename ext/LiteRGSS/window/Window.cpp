85045a77dd296a419986ecc31a914d3ebc9291b3
Ota <stevenotaku17@gmail.com>
2026-03-01 15:36:59 +0100

    Update config window

--- ext/LiteRGSS/window/Window.cpp
+++ ext/LiteRGSS/window/Window.cpp
@@ -3,9 +3,17 @@
 
 VALUE rb_cWindow = Qnil;
 
-VALUE rb_Window_open(VALUE self)
+// open_window(width=640, height=480, title="LiteRGSS")
+VALUE rb_Window_open(int argc, VALUE* argv, VALUE self)
 {
-    raylib::InitWindow(640, 480, "LiteRGSS");
+    VALUE width, height, title;
+    rb_scan_args(argc, argv, "03", &width, &height, &title);
+
+    int   w = RTEST(width)  ? NUM2INT(width)            : 640;
+    int   h = RTEST(height) ? NUM2INT(height)           : 480;
+    const char* t = RTEST(title) ? StringValueCStr(title) : "LiteRGSS";
+
+    raylib::InitWindow(w, h, t);
     return self;
 }
 
@@ -31,8 +39,8 @@ void Init_Window()
 {
     rb_cWindow = rb_define_class_under(rb_mLiteRGSS, "Window", rb_cObject);
 
-    rb_define_method(rb_cWindow, "open_window",    _rbf rb_Window_open,         0);
-    rb_define_method(rb_cWindow, "update",         _rbf rb_Window_update,       0);
-    rb_define_method(rb_cWindow, "close_window",   _rbf rb_Window_close,        0);
-    rb_define_method(rb_cWindow, "should_close?",  _rbf rb_Window_should_close, 0);
+    rb_define_method(rb_cWindow, "open_window",   _rbf rb_Window_open,         -1);
+    rb_define_method(rb_cWindow, "update",        _rbf rb_Window_update,        0);
+    rb_define_method(rb_cWindow, "close_window",  _rbf rb_Window_close,         0);
+    rb_define_method(rb_cWindow, "should_close?", _rbf rb_Window_should_close,  0);
 }
